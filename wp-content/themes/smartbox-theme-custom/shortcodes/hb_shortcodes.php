942828e454cf61a1f3720e2618f84e1e940a0467
bomi1012 <bomi1012@hs-karlsruhe.de>
2014-11-30 18:27:08 +0100

    function hb_get_recent_blog_posts($atts)  wurde überarbeitet

--- wp-content/themes/smartbox-theme-custom/shortcodes/hb_shortcodes.php
+++ wp-content/themes/smartbox-theme-custom/shortcodes/hb_shortcodes.php
@@ -5,21 +5,6 @@ require_once CUSTOM_INCLUDES_DIR . 'hb_utility.php';
  * Custom shortcode functions go here
  * @author Andriy Sobol
  */
-/**
- * @return DIV HTML
- */
-function oxy_shortcode_div($atts, $content = null) {
-    extract(shortcode_atts(array(
-        'class' => '',
-        'id' => ''), $atts));
-
-    $output = '<div id="' . $id . '" class="' . $class . '">';
-    $output .= do_shortcode($content); 
-    $output .= '</div>';
-    return $output;
-}
-add_shortcode('div', 'oxy_shortcode_div');
-
 
 /**
  * @description latest taxonomy topics shown on main page
@@ -62,9 +47,9 @@ function get_latest_taxonomy_topics_as_list($atts) {
                     'link'      => $link,
                     'content'   =>  get_image_as_round_box($taxonomy_image_link)));                
 
-        $output_loop .= oxy_shortcode_div(array('class' => 'well blockquote-well'), $title . $blockquote . $round_link);
+        $output_loop .= oxy_shortcode_layout(NULL, $title . $blockquote . $round_link, 'well blockquote-well');
     }
-    $output = oxy_shortcode_div(array('class' => 'unstyled row-fluid'), $output_loop);
+    $output = oxy_shortcode_layout(NULL, $output_loop, 'unstyled row-fluid');
     return oxy_shortcode_section($atts, $output);
 }
 add_shortcode('latest_taxonomy_topics', 'get_latest_taxonomy_topics_as_list');
@@ -145,6 +130,75 @@ function hb_get_shortcode_blockquote($atts, $content ) {
         'params' => $atts));
 }
 add_shortcode( 'blockquote', 'hb_get_shortcode_blockquote' );
+
+/**
+ * @description shows recents blogs on main page
+ * @global type $post
+ * @param array $atts
+ * @return String
+ */
+function hb_get_recent_blog_posts($atts) {
+    extract(shortcode_atts(array(
+        'title' => '',
+        'style' => '',
+        'src_url' => ''
+                    ), $atts));
+
+    $args = array(
+        'showposts' => 2, // Number of related posts that will be shown.  
+        'orderby' => 'date',
+    );
+    $my_query = new wp_query($args);
+    if ($my_query->have_posts()) {
+        while ($my_query->have_posts()) {
+            global $post;
+            $my_query->the_post();
+            setup_postdata($post);
+            $author_avatar = get_avatar(get_the_author_meta('ID'), 300);
+            $author = get_the_author();
+            $date = get_the_time(get_option("date_format"));
+            $post_link = get_hb_linkformat(get_post_format());    
+
+
+            $div_avatar_left = oxy_shortcode_layout(NULL, $author_avatar, 'round-box box-small');
+            $title_autor_left = get_hb_title(array(
+                    'tag'       => 5,
+                    'class'     => 'text-center',
+                    'content'   => $author));
+            $title_date_left = get_hb_title(array(
+                    'tag'       => 5,
+                    'class'     => 'text-center light',
+                    'content'   => $date));
+            
+            $link_right = get_hb_link(array( 
+                'content'  => get_the_title(),
+                'link' => $post_link));
+            
+            $title_right = get_hb_title(array(
+                    'tag'       => 3,
+                    'content'   => $link_right));
+            
+            $content_right = oxy_limit_excerpt(strip_tags(get_the_content()), 30);
+            $content_right .= get_hb_link(array( 
+                'content'  => get_more_text($post->post_type),
+                'link' => $post_link,
+                'class' => 'more-link'));
+            
+            $text_right = '<p>' . apply_filters('the_content', $content_right) . '</p>';
+            
+            $merge_spans = oxy_shortcode_layout(NULL, $div_avatar_left . $title_autor_left . $title_date_left, 'span3 post-info');
+            $merge_spans .= oxy_shortcode_layout(NULL, $title_right . $text_right, 'span9');
+            $output_loop .= oxy_shortcode_layout(NULL, oxy_shortcode_row(NULL, $merge_spans, NULL), 'span6');
+        }
+    }
+    return oxy_shortcode_section($atts, oxy_shortcode_layout(NULL, $output_loop , 'unstyled row-fluid'));
+}	
+add_shortcode('hb_blog_posts', 'hb_get_recent_blog_posts');
+
+
+
+
+
 
 
 
@@ -372,55 +426,7 @@ add_shortcode('hb_add_into_wrapper', 'hb_add_element_into_wrapper');
 
 
 
-//shows recents blogs on main page
-function hb_get_recent_blog_posts($atts) {
-extract(shortcode_atts(array(
-        'title' => '',
-        'style' => '',
-        'src_url' => ''
-                    ), $atts));
 
- $args = array(
-        'showposts' => 2, // Number of related posts that will be shown.  
-        'orderby' => 'date',
-    );
- $output = '';
-    $my_query = new wp_query($args);
-    if ($my_query->have_posts()){
-        $output .='<ul class="unstyled row-fluid">';
-			while ($my_query->have_posts()) {
-			 global $post;
-			$my_query->the_post();
-			setup_postdata($post);
-				$author_avatar = get_avatar(get_the_author_meta('ID'), 300 );
-				$author  = get_the_author();
-				$date = get_the_time(get_option("date_format"));
-				if ('link' == get_post_format()) {
-                   $post_link = oxy_get_external_link();
-               } else {
-                    $post_link = get_permalink();
-                }
-			
-				$more_text=  get_more_text($post->post_type);
-				$output .= '<li class="span6">';
-				$output .= '<div class="row-fluid"><div class="span3 post-info">';
-				$output .='<div class="round-box box-small">';
-				$output .= $author_avatar. '</div><h5 class="text-center">'.$author.'</h5>';
-				$output .= '<h5 class="text-center light">'.$date.'</h5></div>';
-				$output .= '<div class="span9">';
-				$output .= '<h3><a href="' . $post_link . '"> '. get_the_title() . '</a></h3>';
-			 
-				$content = oxy_limit_excerpt(strip_tags(get_the_content()), 30)  ;
-				$content .='<a href="' . $post_link . '" class="more-link">' . $more_text . '</a>';
-             
-				$output.='<p>' . apply_filters('the_content', $content) . '</p></div></li>';
-			}
-    	}
-	$output .= '</ul>';
-              
-    return oxy_shortcode_section($atts, $output);
-}	
-add_shortcode('hb_blog_posts', 'hb_get_recent_blog_posts');
 
 //used on main pages in dutch and german in order to show latest conten
 function hb_get_recent_oxy_content($atts) {          
