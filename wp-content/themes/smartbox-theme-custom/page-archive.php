9daf92b0c8bd1eb9c7bb748967a4b80538415895
Tamara <toma.gedroit@gmail.com>
2014-12-03 21:24:25 +0100

    pages for archive renamed

--- wp-content/themes/smartbox-theme-custom/page-archive.php
+++ wp-content/themes/smartbox-theme-custom/page-archive.php
@@ -1,57 +1,63 @@
 <?php
-/* 
-Template Name: Archives
-*/
-get_header();
-oxy_page_header();
-$allow_comments = oxy_get_option( 'site_comments' );
+/**
+ * Displays archive for texts. This page is used for requests:
+ * 1 Search reguest of texts(e.g. url =".../?s=searched_keaword&post_type=oxy_content"
+ * 2 Taxonomy request of texts (e.g. url = ".../blog/teaching_topics/golgota/?post_type=oxy_content")
+ * 3 Archive request of texts (e.g. url = ".../blog/2014/10/?post_type=oxy_content")
+ * 4 Display all videos (e.g url = "...?post_type=oxy_content")
+ * @package Smartbox
+ * @subpackage Frontend
+ * @since 0.1
+ *
+ * @copyright (c) 2013 Oxygenna.com
+ * @license http://wiki.envato.com/support/legal-terms/licensing-terms/
+ * @version 1.5
+ */
+if (is_day()) {
+    //$title = __('Day', THEME_FRONT_TD);
+    $title = get_the_date('j M Y');
+} elseif (is_month()) {
+    //$title = __('Month', THEME_FRONT_TD);
+    $title = get_the_date('F Y');
+} elseif (is_year()) {
+    //$title = __('Year', THEME_FRONT_TD);
+    $title = get_the_date('Y');
+} elseif (is_search()) {
+    $title = __('Search', THEME_FRONT_TD) . ': ' . '<span class="lighter">' . get_search_query() . '</span>'; 
+}else {
+    $term = $wp_query->queried_object;
+    $title = $term->name;
+    if ($term->slug == "god") 
+        $title = "";
+}
+
+if(!empty($term->slug))
+    $image = get_taxonomy_banner_image('teaching_topics', $term->slug);
+else 
+    $image = get_taxonomy_banner_image('teaching_topics', 'god');
+
+if(is_page()){
+    get_header();
+    oxy_page_header();
+}else{
+    get_header();
+    oxy_create_hero_section($image, $title);
+}
+
 ?>
-<div id="primary" class="site-content">
-<div id="content" role="main">
+<section class="section section-padded">
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span9">
+                <?php get_template_part('partials/hb_loop'); ?>
+            </div>
+            <aside class="span3 sidebar">
+                <?php dynamic_sidebar('sidebar-texts'); ?>
+            </aside>
+        </div>        
+    </div>
+</section>
+<?php get_footer();
 
-<?php while ( have_posts() ) : the_post(); ?>
-				
-<h1 class="entry-title"><?php the_title(); ?></h1>
 
-<div class="entry-content">
-
-<?php the_content(); ?>
-<!-- Custom Archives Functions Go Below this line -->
-<p><strong>By Date</strong></p>
-<ul>
-<?php compact_archive($style='block'); ?>
-</ul>
-
-<p><strong>Categories:</strong></p>
-<ul class="bycategories">
-<?php wp_list_categories('title_li='); ?>
-</ul>
-<div class="clear"></div>
-
-<p><strong>Tags Cloud:</strong></p>
-<?php wp_tag_cloud(); ?>
-
-<p><strong>Archives:</strong></p>
-<?php wp_get_archives('type=postbypost&limit=10'); ?>
-
-<p><strong>Video Category:</strong></p>
-    <?php
-    $term_id = 4;
-    $taxonomy_name = 'oxy_content_category';
-    $termchildren = get_term_children($term_id, $taxonomy_name);
-
-    echo '<ul>';
-    foreach ($termchildren as $child) {
-        $term = get_term_by('id', $child, $taxonomy_name);
-        echo '<li><a href="' . get_term_link($child, $taxonomy_name) . '">' . $term->name . '</a></li>';
-    }
-    echo '</ul>';
-    ?>
-
-</div><!-- .entry-content -->
-
-<?php endwhile; // end of the loop. ?>
-
-</div><!-- #content -->
-</div><!-- #primary -->
-<?php get_footer(); ?>
+            
