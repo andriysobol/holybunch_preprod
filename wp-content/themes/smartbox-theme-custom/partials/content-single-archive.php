b167b9b99d3404ef0cd8080648a0aa09dc3f8160
Tamara <toma.gedroit@gmail.com>
2014-12-03 19:40:55 +0100

    archive enhancement

--- wp-content/themes/smartbox-theme-custom/partials/content-single-archive.php
+++ wp-content/themes/smartbox-theme-custom/partials/content-single-archive.php
@@ -12,34 +12,33 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('row-fluid'); ?>>    
-    <div class="span12">
-        <div class="span12 post-body">
-            <div class="post-head">
-                <h2 class="small-screen-center">
-                    <?php if (is_single()) : ?>
+    <div class="span12 archive-body">
+        <div class="post-head">
+            <h2 class="small-screen-center">
+                <?php if (is_single()) : ?>
+                    <?php the_title(); ?>
+                <?php else : ?>
+                    <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr(sprintf(__('Permalink to %s', THEME_FRONT_TD), the_title_attribute('echo=0'))); ?>" rel="bookmark">
                         <?php the_title(); ?>
-                    <?php else : ?>
-                        <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr(sprintf(__('Permalink to %s', THEME_FRONT_TD), the_title_attribute('echo=0'))); ?>" rel="bookmark">
-                            <?php the_title(); ?>
-                        </a>
-                    <?php endif; // is_single() ?>
-                </h2>
-                <?php get_template_part('partials/post-extras'); ?>
-            </div>
-            <div class="entry-content">
-                <?php
-                $more_text = get_more_text($post->post_type);
-                $link = get_permalink();
-                if (is_search()):
-                    $output = relevanssi_the_excerpt();
-                    $output .= oxy_shortcode_accordions('','[accordion title="Краткое описание статьи"]'.get_post_summary_mini($post).'[/accordion]');
-                    $output .= '<a href="' . $link . '" class="more-link">' . $more_text . '</a>';
-                else:
-                    $content = get_post_summary_mini($post); 
-                    $content .= '<a href="' . $link . '" class="more-link">' . $more_text . '</a>';
-                    $output = apply_filters('the_content', $content);
-                endif;
-                echo $output;
-                ?>
-            </div>
-            </article>
+                    </a>
+                <?php endif; ?>
+            </h2>
+        </div>
+        <div class="entry-content">
+            <?php
+            $content_right .= get_hb_link(array(
+                'content' => get_more_text($post->post_type),
+                'link' => get_permalink(),
+                'class' => 'more-link'));
+
+            if (is_search()):
+                $output = relevanssi_the_excerpt() . $content_right;
+            else:
+                $content = oxy_limit_excerpt(strip_tags(get_the_content()), 55) . $content_right;
+                $output = apply_filters('the_content', $content);
+            endif;
+            echo $output;
+            ?>
+        </div>
+    </div>
+</article>
--- wp-content/themes/smartbox-theme-custom/partials/video-single-archive.php
+++ wp-content/themes/smartbox-theme-custom/partials/video-single-archive.php
@@ -14,31 +14,12 @@
 global $post;
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('row-fluid'); ?>>    
-    <div class="span12">
-        <div class="span12 post-body">
-            <div class="post-head">
-                <h2 class="small-screen-center">
-                    <?php if (is_single()) : ?>
-                        <?php the_title(); ?>
-                    <?php else : ?>
-                        <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr(sprintf(__('Permalink to %s', THEME_FRONT_TD), the_title_attribute('echo=0'))); ?>" rel="bookmark">
-                            <?php the_title(); ?>
-                        </a>
-                    <?php endif;  ?>
-                </h2>
-            </div>
-            <div class="entry-content">
-                <?php
-                if (is_search()):
-                    $content = relevanssi_the_excerpt();
-                else:
-                    $content = get_the_content();
-                endif;
-                $video_shortcode = get_field('video_shortcode', $post->ID);
-                $output = '<div>' . create_videowrapper_div($video_shortcode, $span = "span12", "1250", "703") . '</div>';
-                $output .= '<div class="span12" style="margin-top: 25px;">' . $content . '</div>';
-                echo $output;
-                ?>
-            </div>
+    <div class="span12 archive-body">
+        <div class="entry-content">
+            <?php
+            $video_shortcode = get_field('video_shortcode', $post->ID);
+            echo create_videowrapper_div($video_shortcode, $span = "span12", "1250", "703");
+            ?>
         </div>
+    </div>
 </article>
